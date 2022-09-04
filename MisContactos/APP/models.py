387e8198b6dbdd3e9bdc84c4127f7bae53d5418e
Felipe Cardenas <fcardenasjara8@gmail.com>
2022-09-04 18:33:01 -0300

    migraciones

--- MisContactos/APP/models.py
+++ MisContactos/APP/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Familiares(models.Model):
+    nombre = models.CharField(max_length=30)
+    email = models.EmailField()
+    edad = models.IntegerField()
+    registro = models.DateField()
+
+class Amigos(models.Model):
+    nombre = models.CharField(max_length=30)
+    email = models.EmailField()
+    edad = models.IntegerField()
+    registro = models.DateField()
+
+class Colegas(models.Model):
+    nombre = models.CharField(max_length=30)
+    email = models.EmailField()
+    edad = models.IntegerField()
+    registro = models.DateField()
