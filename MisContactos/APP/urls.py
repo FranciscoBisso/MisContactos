a9536b657042c26d1b0f048f70a0506d88b032ba
FranciscoBisso <fjbisso@gmail.com>
2022-09-04 21:38:07 -0300

    agrego views, urls y templates y más

--- MisContactos/APP/urls.py
+++ MisContactos/APP/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from APP.views import inicio
+from APP.views import *
 
 urlpatterns = [
     path('', inicio, name='inicio'),
+    path('agregar_familiar', agregarFamiliar, name='agregarFamiliar'),
+    path('agregar_amigo', agregarAmigo, name='agregarAmigo'),
+    path('agregar_colega', agregarColega, name='agregarColega'),
 ]
--- MisContactos/APP/views.py
+++ MisContactos/APP/views.py
@@ -1,6 +1,88 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from APP.models import *
+from APP.forms import *
 
 # Create your views here.
 def inicio(req):
     # futura carátula de la página
     return render(req, 'index.html')
+
+def agregarFamiliar(req):
+    # 2) chequeo que la info venga por verbo POST
+    if req.method == 'POST':
+        formulario = FormularioPersonas(req.POST)
+        
+        # 3) chequeo validaciones de los datos que recibo del formulario
+        if formulario.is_valid():
+            # 4) limpio el formulario
+            data = formulario.cleaned_data
+            
+            # 5) inserto datos y guardo en db
+            familiar = Familiares(nombre=data.get('nombre'), email=data.get('email'), edad=data.get('edad'), registro=data.get('registro'))
+            familiar.save(data)
+            
+            messages.info(req,
+                  f'Se ha agregado a: {familiar.nombre} a tus familiares.')
+            
+            # 6) redirijo a 
+            return redirect('inicio')
+    
+    # 1) cargo el formulario vacio para que sea completado por el usuario
+    contexto = {
+        'form': FormularioPersonas()
+    }
+    return render(req, 'formulario.html', contexto)
+
+def agregarAmigo(req):
+    # 2) chequeo que la info venga por verbo POST
+    if req.method == 'POST':
+        formulario = FormularioPersonas(req.POST)
+        
+        # 3) chequeo validaciones de los datos que recibo del formulario
+        if formulario.is_valid():
+            # 4) limpio el formulario
+            data = formulario.cleaned_data
+            
+            # 5) inserto datos y guardo en db
+            amigo = Amigos(nombre=data.get('nombre'), email=data.get('email'), edad=data.get('edad'), registro=data.get('registro'))
+            amigo.save(data)
+            
+            messages.info(req,
+                  f'Se ha agregado a: {amigo.nombre} a tus amigos.')
+            
+            # 6) redirijo a 
+            return redirect('inicio')
+    
+    # 1) cargo el formulario vacio para que sea completado por el usuario
+    contexto = {
+        'form': FormularioPersonas()
+    }
+    return render(req, 'formulario.html', contexto)
+
+
+def agregarColega(req):
+    # 2) chequeo que la info venga por verbo POST
+    if req.method == 'POST':
+        formulario = FormularioPersonas(req.POST)
+        
+        # 3) chequeo validaciones de los datos que recibo del formulario
+        if formulario.is_valid():
+            # 4) limpio el formulario
+            data = formulario.cleaned_data
+            
+            # 5) inserto datos y guardo en db
+            colega = Colegas(nombre=data.get('nombre'), email=data.get('email'), edad=data.get('edad'), registro=data.get('registro'))
+            colega.save(data)
+            
+            messages.info(req,
+                  f'Se ha agregado a: {colega.nombre} a tus colegas.')
+            
+            # 6) redirijo a 
+            return redirect('inicio')
+    
+    # 1) cargo el formulario vacio para que sea completado por el usuario
+    contexto = {
+        'form': FormularioPersonas()
+    }
+    return render(req, 'formulario.html', contexto)
