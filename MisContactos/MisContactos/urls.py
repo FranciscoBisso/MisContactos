205424014e7f6ae9946c313d403e379d25a24411
FranciscoBisso <fjbisso@gmail.com>
2022-09-05 19:08:35 -0300

    agrego path lambda a MisContactos/urls.py

--- MisContactos/MisContactos/urls.py
+++ MisContactos/MisContactos/urls.py
@@ -1,8 +1,11 @@
 
 from django.contrib import admin
 from django.urls import path,include
+from django.shortcuts import redirect
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('APP/', include('APP.urls')),
+    path('', lambda req: redirect('inicio')),
 ]
